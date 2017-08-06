c3910ddc0d81ec9f72a6260ff8cf94f726f0129d
Mark Neuburger <markneub@gmail.com>
2017-08-06 00:27:25 -0700

    Fix

--- src/AnimatedCircularProgress.js
+++ src/AnimatedCircularProgress.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { View, Animated } from 'react-native';
 import CircularProgress from './CircularProgress';
